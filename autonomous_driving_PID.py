bc8cf467489914012eb766d128add8880d32453b
Rishabh Verma <64482413+RiVer2000@users.noreply.github.com>
2024-05-09 15:37:19 -0400

    Update autonomous_driving_PID.py

--- autonomous_driving_PID.py
+++ autonomous_driving_PID.py
@@ -12,7 +12,7 @@ try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
-        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+        'linux-x86_64'))[0])
 except IndexError:
     pass
 
